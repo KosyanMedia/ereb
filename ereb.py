073a6b47f448686134418817bd7b7b8f43aa6b63
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2015-11-18 19:07:05 +0700

    fix(ereb.py): massive load time optimization for /tasks

--- ereb.py
+++ ereb.py
@@ -12,7 +12,8 @@ class TasksHandler(tornado.web.RequestHandler):
     def get(self, task_id, action, task_run_id):
         result = '404'
         if task_id == '' and action == '':
-            result = json.dumps(self.task_controller.get_task_list(with_history=True))
+            tasks = self.task_controller.get_task_list(with_history=True)
+            result = json.dumps(list(map(lambda x: x if x.update({'runs': [x['runs'][0]] + list(map(lambda y: {'state':{'exit_code': y['state']['exit_code']}}, x['runs'][1:]))}) is None else x, tasks)))
         elif task_id != '':
             task = self.task_controller.get_task_by_id(task_id)
             if not task:

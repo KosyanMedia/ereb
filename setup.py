428abc878f6a99ecfd5b1bf94e6bf27d25e394dd
Aleksei Shaikhaleev <nimdraug.sael@gmail.com>
2017-01-23 14:47:19 +0700

    requirements fix, version update

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='ereb',
     description='Tornado based cron with web interface, JSON API and history',
-    version='0.17.2',
+    version='0.17.3',
     url='https://github.com/KosyanMedia/ereb',
     author='Alex Shaikhaleev',
     author_email='nimdraug.sael@gmail.com',
     keywords='tornado web cron',
     packages=['ereb'],
-    install_requires=['tornado==4.3', 'crontab', 'psutil'],
+    install_requires=['tornado==4.3', 'crontab', 'psutil', 'requests'],
     include_package_data=True,
     license='MIT',
     entry_points="""
